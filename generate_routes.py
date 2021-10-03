10906d779ea2cea867dc332ec789f4b1b24c663e
ccor065 <ccor065@aucklanduni.ac.nz>
2021-10-03 19:51:13 +1300

    Changed generation so that it produces a vector for each day, added correesponding cost vector for each route

--- generate_routes.py
+++ generate_routes.py
@@ -5,22 +5,8 @@ import itertools
 This file generates feasible sub-routes per region.
 """
 durations = pd.read_csv('assignment_resources/WoolworthsTravelDurations.csv')
-print(durations.head())
-## Read in store data
-stores_df = pd.read_csv('stores_df.csv')
-## region names
-region_names = np.array(["Central Region","South Region","North Region","East Region","West Region","Southern Most Region"])
 
-## Construct Data frame for each region.
-central =  stores_df[stores_df.loc[:]["Region"] == region_names[0]]
-south =  stores_df[stores_df.loc[:]["Region"] == region_names[1]]
-north =  stores_df[stores_df.loc[:]["Region"] == region_names[2]]
-east =  stores_df[stores_df.loc[:]["Region"] == region_names[3]]
-west =  stores_df[stores_df.loc[:]["Region"] == region_names[4]]
-southernMost =  stores_df[stores_df.loc[:]["Region"] == region_names[5]]
-
-
-
+'''
 def generate_routes(region, min, max):
     """
     This function generates sub-routes within a given region and checks the feasiblity of a sub-route based
@@ -107,45 +93,119 @@ def generate_routes(region, min, max):
                 ds += z.Saturday.values[0]
             #Check feaislblity
             if ds <= 26:
-                # ad duration to end of subset
+                # add duration to end of subset
                 subset += (getDuration(subset),)
                 sc.append(subset)
     # return fealisble sub-routes for each day.
     return [mc, tuc, wc,thc,fc,sc]
+'''
+def generate_routes(stores_df, region_names):
+    min = 2
+    max = 5
+    # intialise lists that store valid sub-routes per day
+    monday_routes = []         #monday
+    tuesday_routes = []        #tuesday .. etc
+    wednesday_routes = []
+    thursday_routes = []
+    friday_routes = []
+    saturday_routes = []
+    # all routes must begin at disrubtion centre
+    dc = 'Distribution Centre Auckland'
+    # generates sub-routes per day
+    """ WEEK DAY ROUTES"""
+    for i in range(len(region_names)):
+        region = stores_df[stores_df.loc[:]["Region"] == region_names[i]]
+        region_stores = np.array(region.Store) 
+        ## Loop through number of stores per sub-route
+        for i in range (min, max):
+            # get every possible combintaion of stores based on how many are in each route.
+            for subset in itertools.combinations(region_stores, i):
+                # initalise counters, these count demands for routes.
+                
+                dm = 0
+                dtu = 0
+                dw = 0
+                dth = 0
+                df = 0
+                # cycle through each store in the sub-route and sum their demands.
+                for node in subset:
+                    # add to counter based on day/
+                    z = region[region.Store == node]
+                    dm += z.Monday.values[0]
+                    dtu += z.Tuesday.values[0]
+                    dw += z.Wednesday.values[0]
+                    dth += z.Thursday.values[0]
+                    df += z.Friday.values[0]
+                
+                # Add duration to end of sub-route
+                subset = (dc,) + subset + (dc,) 
+                # Check freasblity of specfic route
+                if dm <= 26:
+                    
+                    monday_routes.append(subset)
+                if dtu <= 26:
+                    tuesday_routes.append(subset)
+                if dw <= 26:
+                    wednesday_routes.append(subset)
+                if dth <= 26:
+                    thursday_routes.append(subset)
+                if df <= 26:
+                    friday_routes.append(subset)
+        #Generate saturdays
+        satC_stores = []
+        # Add stores to list
+        for store in region_stores:
+            z = region[region.Store == store]
+            if z.Saturday.values[0] != 0:
+                satC_stores.append(store)
+        #Generate sub-routes say way as week-day
+        for i in range (min,max):
+            for subset in itertools.combinations(satC_stores, i):
+                ds = 0 #Initalse demand counter
+                for node in subset:
+                    z = region[region.Store == node]
+                    ds += z.Saturday.values[0]
+                #Check feaislblity
+                if ds <= 26:
+                    # add duration to end of subset
+                    subset = (dc,) + subset + (dc,) 
+                    saturday_routes.append(subset)
+
+    # return fealisble sub-routes for each day.
 
 
-def getDuration(route):
+    return  monday_routes, tuesday_routes, wednesday_routes,thursday_routes, friday_routes,saturday_routes
+
+
+def getCosts(route_set):
     """
     This function calculates the duration of a specfic route
+
     """
-    duration=0
-    for i in range(len(route)-1):
-        currentStore = route[i]
-        nextStore = route[i+1]
-        row = durations.loc[durations['Store'] == currentStore]
-        duration += row[nextStore].values[0]
-    return duration
+    cost_set =[]
+    for route in route_set:
+        nStore = len(route)
+        duration=450*nStore
+        cost =0
+        for i in range(len(route)-1):
+            currentStore = route[i]
+            nextStore = route[i+1]
+            row = durations.loc[durations['Store'] == currentStore]
+            duration += row[nextStore].values[0]
 
-''' get number of sotes in each region
-print(len(north.index))
-print(len(south.index))
-print(len(east.index))
-print(len(west.index))
-print(len(southernMost.index))
-'''
+        #less than four hours
+        if duration <= 14400:
+            cost = duration*0.0625
+        if duration > 14400:
+            cost = 14400*0.0625
+            extraTime = duration - 14400
+            cost += extraTime * (275/3600)
+        cost_set.append(cost)
+    return cost_set
 
 
-central_routes = generate_routes(central, 2, 4) #number of stores 17
 
-print(central_routes[0][0])
-'''
-northern_routes = generate_routes(north, 2, 4) #9 stores
-southern_routes = generate_routes(south, 2, 4) #9 stores
-eastern_routes = generate_routes(east, 2, 4) #12 stores
-western_routes = generate_routes(west, 2, 4) # 14 stores
-southern_routes= generate_routes(southernMost, 2, 4) #4 stores
 
-'''
 
 
 
--- set_partiton.py
+++ set_partiton.py
@@ -1,3 +1,44 @@
 import pulp
+import pandas as pd
+import numpy as np
+from generate_routes import *
 
-#
+# This file creates a set parttion
+
+## Read in store data
+stores_df = pd.read_csv('stores_df.csv')
+## region names
+region_names = np.array(["Central Region","South Region","North Region","East Region","West Region","Southern Most Region"])
+monday, tuesday, wednesday, thursday, friday,saturday = generate_routes(stores_df, region_names)
+mon_costs = getCosts(monday)
+tueCosts = getCosts(tuesday)
+wedCosts = getCosts(wednesday)
+thurCosts = getCosts(thursday)
+friCosts = getCosts(friday)
+satCosts = getCosts(saturday)
+## Test
+print(len(satCosts) == len(saturday))
+print(len(tueCosts) == len(tuesday))
+print(len(mon_costs) == len(monday))
+print(len(wedCosts) == len(wednesday))
+print(len(thurCosts) == len(thursday))
+print(len(friCosts) == len(friday))
+
+weekday_stores = stores_df['Store']
+saturday_stores = stores_df.loc[stores_df['Saturday']!=0,  ['Store']]
+nTrucks = 30
+'''
+x = pulp.LpVariable.dicts('Monday', monday, 
+                            lowBound = 0,
+                            upBound = 1,
+                            cat = pulp.LpInteger)
+
+mon_routing = pulp.LpProblem("Monday Routing", pulp.LpMinimize)
+
+mon_routing += sum([x[route] for route in monday]) <= nTrucks, "Maximum_number_of_trucks"
+#Store should only be visited once
+for store in weekday_stores:
+    mon_routing += sum([x[route] for route in monday
+                                if store in route]) == 1, "storez %s"%store
+mon_routing.solve()
+'''
